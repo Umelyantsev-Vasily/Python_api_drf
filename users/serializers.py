5d6122158512f5529d03ee8ef0d6621c97ee670a
tanec <tanec_991@mail.ru>
2025-10-08 21:45:40 +1000

    32.2 Документирование и безопасность, доработка Users 2.0

--- users/serializers.py
+++ users/serializers.py
@@ -1,26 +1,22 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from rest_framework.validators import UniqueValidator
-#
-# from .models import Payment
-
-
-# class PaymentSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Payment
-#         fields = '__all__'
-
 
 User = get_user_model()
 
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
+    password = serializers.CharField(write_only=True, min_length=8)
     password_confirm = serializers.CharField(write_only=True)
 
     class Meta:
         model = User
         fields = ('email', 'password', 'password_confirm', 'phone', 'city')
+        extra_kwargs = {
+            'email': {'required': True},
+            'password': {'required': True},
+            'password_confirm': {'required': True}
+        }
 
     def validate(self, attrs):
         if attrs['password'] != attrs['password_confirm']:
@@ -29,7 +25,8 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         validated_data.pop('password_confirm')
-        return User.objects.create_user(**validated_data)
+        user = User.objects.create_user(**validated_data)
+        return user
 
 
 class UserProfileSerializer(serializers.ModelSerializer):
--- users/views.py
+++ users/views.py
@@ -1,10 +1,11 @@
+from .permissions import IsOwnerOrStaff
+from .serializers import UserProfileSerializer
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from django.contrib.auth import get_user_model
-from .serializers import UserSerializer, UserRegistrationSerializer, UserProfileSerializer
-from .permissions import IsOwnerOrStaff
+from .serializers import UserSerializer, UserRegistrationSerializer
 
 User = get_user_model()
 
@@ -14,25 +15,39 @@ class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
 
     def get_permissions(self):
-        # Разрешаем доступ без аутентификации только для создания (регистрации)
-        if self.action == 'create':
+        # Разные разрешения для разных действий
+        if self.action == 'create' or self.action == 'register':
             return [AllowAny()]
-        # Для всех остальных действий требуем аутентификацию
-        return [IsAuthenticated(), IsOwnerOrStaff()]
+        elif self.action in ['update', 'partial_update', 'destroy']:
+            return [IsAuthenticated(), IsOwnerOrStaff()]
+        else:
+            # Для list, retrieve - только аутентифицированные
+            return [IsAuthenticated()]
 
     def get_queryset(self):
-        if self.request.user.is_staff:
+        if self.request.user.is_authenticated and self.request.user.is_staff:
             return User.objects.all()
-        return User.objects.filter(id=self.request.user.id)
+        elif self.request.user.is_authenticated:
+            return User.objects.filter(id=self.request.user.id)
+        else:
+            return User.objects.none()
 
     def update(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return Response(
+                {"detail": "Authentication credentials were not provided."},
+                status=status.HTTP_401_UNAUTHORIZED
+            )
+
         if int(kwargs['pk']) != request.user.id and not request.user.is_staff:
-            return Response({"detail": "Нет прав для редактирования этого профиля"},
-                            status=status.HTTP_403_FORBIDDEN)
+            return Response(
+                {"detail": "Нет прав для редактирования этого профиля"},
+                status=status.HTTP_403_FORBIDDEN
+            )
         return super().update(request, *args, **kwargs)
 
     def get_serializer_class(self):
-        if self.action == 'create':
+        if self.action == 'create' or self.action == 'register':
             return UserRegistrationSerializer
         elif self.action in ['update', 'partial_update']:
             return UserSerializer
@@ -41,12 +56,13 @@ class UserViewSet(viewsets.ModelViewSet):
 
     @action(detail=False, methods=['post'], permission_classes=[AllowAny])
     def register(self, request):
-        """Эндпоинт для регистрации (можно удалить, если используете стандартный create)"""
+        """Регистрация нового пользователя"""
         serializer = UserRegistrationSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = serializer.save()
         return Response({
             'message': 'Пользователь успешно зарегистрирован',
-            'user_id': user.id
+            'user_id': user.id,
+            'email': user.email
         }, status=status.HTTP_201_CREATED)
 
