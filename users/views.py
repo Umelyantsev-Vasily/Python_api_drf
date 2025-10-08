79a08bb58bf1d589e2d2c13dffa0aa95b0db1c61
tanec <tanec_991@mail.ru>
2025-10-08 21:27:47 +1000

    32.2 Документирование и безопасность, доработка Users

--- users/views.py
+++ users/views.py
@@ -1,22 +1,23 @@
-from .permissions import IsOwnerOrStaff
-from .serializers import UserProfileSerializer
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from django.contrib.auth import get_user_model
-from .serializers import UserSerializer, UserRegistrationSerializer
+from .serializers import UserSerializer, UserRegistrationSerializer, UserProfileSerializer
+from .permissions import IsOwnerOrStaff
 
 User = get_user_model()
+
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = [IsAuthenticated, IsOwnerOrStaff]
 
     def get_permissions(self):
+        # Разрешаем доступ без аутентификации только для создания (регистрации)
         if self.action == 'create':
             return [AllowAny()]
+        # Для всех остальных действий требуем аутентификацию
         return [IsAuthenticated(), IsOwnerOrStaff()]
 
     def get_queryset(self):
@@ -27,7 +28,7 @@ class UserViewSet(viewsets.ModelViewSet):
     def update(self, request, *args, **kwargs):
         if int(kwargs['pk']) != request.user.id and not request.user.is_staff:
             return Response({"detail": "Нет прав для редактирования этого профиля"},
-                          status=status.HTTP_403_FORBIDDEN)
+                            status=status.HTTP_403_FORBIDDEN)
         return super().update(request, *args, **kwargs)
 
     def get_serializer_class(self):
@@ -40,6 +41,7 @@ class UserViewSet(viewsets.ModelViewSet):
 
     @action(detail=False, methods=['post'], permission_classes=[AllowAny])
     def register(self, request):
+        """Эндпоинт для регистрации (можно удалить, если используете стандартный create)"""
         serializer = UserRegistrationSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = serializer.save()
